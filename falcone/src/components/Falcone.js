3afafce601696696d30b94a955b2f5e1f4e32e06
Sourav-36 <souravnayak.nayak@gmail.com>
2023-09-24 11:29:18 +0530

    Add radio-buttons and dropdowns

--- falcone/src/components/Falcone.js
+++ falcone/src/components/Falcone.js
@@ -7,7 +7,19 @@ const Falcone = () => {
   let [planets, setPlanets] = useState([]);
   let [vehicles, setVehicles] = useState([]);
   let [selected, setSelected] = useState(null);
-  // let [vehicles2, setVehicles2] = useState([]);
+
+  let [planets2, setPlanets2] = useState([]);
+  let [vehicles2, setVehicles2] = useState([]);
+  let [selected2, setSelected2] = useState(null);
+
+  let [planets3, setPlanets3] = useState([]);
+  let [vehicles3, setVehicles3] = useState([]);
+  let [selected3, setSelected3] = useState(null);
+
+  let [planets4, setPlanets4] = useState([]);
+  let [vehicles4, setVehicles4] = useState([]);
+  let [selected4, setSelected4] = useState(null);
+
   let [formData, setFormData] = useState({
     token: "",
     planet_names: [],
@@ -30,8 +42,7 @@ const Falcone = () => {
     })();
   }, []);
 
-  console.log(formData);
-  console.log(planets);
+  // console.log(formData);
 
   return (
     <>
@@ -46,11 +57,6 @@ const Falcone = () => {
             name="planets"
             id="1"
             onChange={async (e) => {
-              let newPlanetsList = [];
-              planets.forEach((data) => {
-                if (data.name !== e.target.value) newPlanetsList.push(data);
-              });
-
               let planetNames = [];
               planetNames.push(e.target.value);
 
@@ -110,13 +116,253 @@ const Falcone = () => {
                     });
 
                     setVehicles(newVehicles);
+
                     let presentPlanetList = [];
                     planets.forEach((obj) => {
                       if (!formData.planet_names.includes(obj.name))
                         presentPlanetList.push(obj);
                     });
 
-                    setPlanets(presentPlanetList);
+                    setPlanets2(presentPlanetList);
+                  }}
+                />
+                <label>{`${obj.name} (${obj.total_no})`}</label>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="each-dropdown-layout">
+          <label>Destination 2</label>
+          <select
+            name="planets2"
+            id="2"
+            onChange={async (e) => {
+              let planetNames = [];
+              formData.planet_names.forEach((data) => {
+                planetNames.push(data);
+              });
+              planetNames.push(e.target.value);
+
+              setFormData({
+                ...formData,
+                planet_names: planetNames,
+              });
+
+              setVehicles2(vehicles);
+            }}
+          >
+            <option value="" default>
+              Select
+            </option>
+            {planets2.map((obj) => {
+              return (
+                <option key={obj.name} id={`${obj.name}`} value={`${obj.name}`}>
+                  {obj.name}
+                </option>
+              );
+            })}
+          </select>
+
+          {vehicles2.map((obj, ind) => {
+            return (
+              <div key={ind}>
+                <input
+                  type="radio"
+                  name="vehicles2"
+                  value={`${obj.name}`}
+                  checked={ind === selected2}
+                  onChange={(e) => {
+                    setSelected2((prev) => {
+                      return ind === prev ? null : ind;
+                    });
+
+                    let vehicleList = [];
+                    formData.vehicle_names.forEach((data) => {
+                      vehicleList.push(data);
+                    });
+                    vehicleList.push(e.target.value);
+                    setFormData({
+                      ...formData,
+                      vehicle_names: vehicleList,
+                    });
+
+                    let newVehicles = [];
+                    vehicles2.forEach((obj) => {
+                      if (obj.name === e.target.value) {
+                        let obj1 = { ...obj };
+                        if (obj1.total_no > 0) obj1.total_no -= 1;
+                        newVehicles.push(obj1);
+                      } else {
+                        newVehicles.push(obj);
+                      }
+                    });
+
+                    setVehicles2(newVehicles);
+                    let presentPlanetList = [];
+                    planets2.forEach((obj) => {
+                      if (!formData.planet_names.includes(obj.name))
+                        presentPlanetList.push(obj);
+                    });
+
+                    setPlanets3(presentPlanetList);
+                  }}
+                />
+                <label>{`${obj.name} (${obj.total_no})`}</label>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="each-dropdown-layout">
+          <label>Destination 3</label>
+          <select
+            name="planets3"
+            id="3"
+            onChange={async (e) => {
+              let planetNames = [];
+              formData.planet_names.forEach((data) => {
+                planetNames.push(data);
+              });
+              planetNames.push(e.target.value);
+
+              setFormData({
+                ...formData,
+                planet_names: planetNames,
+              });
+
+              setVehicles3(vehicles2);
+            }}
+          >
+            <option value="" default>
+              Select
+            </option>
+            {planets3.map((obj) => {
+              return (
+                <option key={obj.name} id={`${obj.name}`} value={`${obj.name}`}>
+                  {obj.name}
+                </option>
+              );
+            })}
+          </select>
+
+          {vehicles3.map((obj, ind) => {
+            return (
+              <div key={ind}>
+                <input
+                  type="radio"
+                  name="vehicles3"
+                  value={`${obj.name}`}
+                  checked={ind === selected3}
+                  onChange={(e) => {
+                    setSelected3((prev) => {
+                      return ind === prev ? null : ind;
+                    });
+
+                    let vehicleList = [];
+                    formData.vehicle_names.forEach((data) => {
+                      vehicleList.push(data);
+                    });
+                    vehicleList.push(e.target.value);
+                    setFormData({
+                      ...formData,
+                      vehicle_names: vehicleList,
+                    });
+
+                    let newVehicles = [];
+                    vehicles3.forEach((obj) => {
+                      if (obj.name === e.target.value) {
+                        let obj1 = { ...obj };
+                        if (obj1.total_no > 0) obj1.total_no -= 1;
+                        newVehicles.push(obj1);
+                      } else {
+                        newVehicles.push(obj);
+                      }
+                    });
+
+                    setVehicles3(newVehicles);
+                    let presentPlanetList = [];
+                    planets3.forEach((obj) => {
+                      if (!formData.planet_names.includes(obj.name))
+                        presentPlanetList.push(obj);
+                    });
+
+                    setPlanets4(presentPlanetList);
+                  }}
+                />
+                <label>{`${obj.name} (${obj.total_no})`}</label>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="each-dropdown-layout">
+          <label>Destination 4</label>
+          <select
+            name="planets4"
+            id="4"
+            onChange={async (e) => {
+              let planetNames = [];
+              formData.planet_names.forEach((data) => {
+                planetNames.push(data);
+              });
+              planetNames.push(e.target.value);
+
+              setFormData({
+                ...formData,
+                planet_names: planetNames,
+              });
+
+              setVehicles4(vehicles3);
+            }}
+          >
+            <option value="" default>
+              Select
+            </option>
+            {planets4.map((obj) => {
+              return (
+                <option key={obj.name} id={`${obj.name}`} value={`${obj.name}`}>
+                  {obj.name}
+                </option>
+              );
+            })}
+          </select>
+
+          {vehicles4.map((obj, ind) => {
+            return (
+              <div key={ind}>
+                <input
+                  type="radio"
+                  name="vehicles4"
+                  value={`${obj.name}`}
+                  checked={ind === selected4}
+                  onChange={(e) => {
+                    setSelected4((prev) => {
+                      return ind === prev ? null : ind;
+                    });
+
+                    let vehicleList = [];
+                    formData.vehicle_names.forEach((data) => {
+                      vehicleList.push(data);
+                    });
+                    vehicleList.push(e.target.value);
+                    setFormData({
+                      ...formData,
+                      vehicle_names: vehicleList,
+                    });
+
+                    let newVehicles = [];
+                    vehicles4.forEach((obj) => {
+                      if (obj.name === e.target.value) {
+                        let obj1 = { ...obj };
+                        if (obj1.total_no > 0) obj1.total_no -= 1;
+                        newVehicles.push(obj1);
+                      } else {
+                        newVehicles.push(obj);
+                      }
+                    });
+
+                    setVehicles4(newVehicles);
                   }}
                 />
                 <label>{`${obj.name} (${obj.total_no})`}</label>
